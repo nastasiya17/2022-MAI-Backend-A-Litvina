562820070ba224b4ddeb4bc728187a94202876a7
nastasiya17 <59894138+nastasiya17@users.noreply.github.com>
2022-06-02 00:43:08 +0300

    Update urls.py

--- lab3/project/project/urls.py
+++ lab3/project/project/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('index', views.index, name='home'),   
-    path('profile', views.profile, name='profile'), 
-    path('showplaces/', views.showplaces, name='showplaces'),    
-    path('showplaces/<int:showplace_id>/', views.showplaces, name='showplaces'),
     path('admin/', admin.site.urls),
+    path('api/', include('myapp.urls')),
 ]
